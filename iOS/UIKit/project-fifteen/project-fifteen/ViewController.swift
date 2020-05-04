8a77d30f73b9eebbaa967cc9e9f70cc96b8130a7
Pamela <pamela.novalli@gmail.com>
2020-05-03 23:48:31 -0300

    Added project

---
+++ iOS/UIKit/project-fifteen/project-fifteen/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  project-fifteen
+//
+//  Created by Pamela Ianovalli on 03/05/20.
+//  Copyright © 2020 Pamela Ianovalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
+            animations: {
+              switch self.currentAnimation {
+                case 0:
+                    self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                
+                case 1:
+                    self.imageView.transform = .identity
+                
+                case 2:
+                    self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                
+                case 3:
+                    self.imageView.transform = .identity
+                
+                case 4:
+                    self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                
+                case 5:
+                    self.imageView.transform = .identity
+                
+                case 6:
+                    self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = UIColor.green
+                
+                case 7:
+                    self.imageView.alpha = 1
+                    self.imageView.backgroundColor = UIColor.clear
+                
+                default:
+                    break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+    
+}
+
