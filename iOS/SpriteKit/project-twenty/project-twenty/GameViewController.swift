91c1f47828955264a6bf0f14e071f199a2d86a50
Pamela <pamela.novalli@gmail.com>
2020-05-16 22:04:36 -0300

    Added game

---
+++ iOS/SpriteKit/project-twenty/project-twenty/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  project-twenty
+//
+//  Created by Pamela Ianovalli on 16/05/20.
+//  Copyright © 2020 Pamela Ianovalli. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
