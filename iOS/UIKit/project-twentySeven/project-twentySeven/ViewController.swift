007d696c7703e124a9a6683c7afce2954c9fcec2
Pamela <pamela.novalli@gmail.com>
2020-06-04 23:04:15 -0300

    Added drawImagesAndText

--- iOS/UIKit/project-twentySeven/project-twentySeven/ViewController.swift
+++ iOS/UIKit/project-twentySeven/project-twentySeven/ViewController.swift
@@ -37,6 +37,8 @@ class ViewController: UIViewController {
             drawRotatedSquares()
         case 4:
             drawLines()
+        case 5:
+            drawImagesAndText()
         default:
             break
         }
@@ -61,18 +63,18 @@ class ViewController: UIViewController {
     
     func drawCircle() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-
+        
         let img = renderer.image { ctx in
             let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
-
+            
             ctx.cgContext.setFillColor(UIColor.red.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.setLineWidth(10)
-
+            
             ctx.cgContext.addEllipse(in: rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
         }
-
+        
         imageView.image = img
     }
     
@@ -117,32 +119,59 @@ class ViewController: UIViewController {
     
     func drawLines() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-
+        
         let img = renderer.image { ctx in
             ctx.cgContext.translateBy(x: 256, y: 256)
-
+            
             var first = true
             var length: CGFloat = 256
-
+            
             for _ in 0 ..< 256 {
                 ctx.cgContext.rotate(by: .pi / 2)
-
+                
                 if first {
                     ctx.cgContext.move(to: CGPoint(x: length, y: 50))
                     first = false
                 } else {
                     ctx.cgContext.addLine(to: CGPoint(x: length, y: 50))
                 }
-
+                
                 length *= 0.99
             }
-
+            
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.strokePath()
         }
-
+        
         imageView.image = img
     }
-
+    
+    func drawImagesAndText() {
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            
+            let paragraphSyle = NSMutableParagraphStyle()
+            paragraphSyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 36),
+                .paragraphStyle: paragraphSyle
+            ]
+            
+            let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+            let atributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            atributedString.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
+            
+            let mouse = UIImage(named: "mouse")
+            mouse?.draw(at: CGPoint(x: 300, y: 150))
+        }
+        
+        imageView.image = img
+    }
+    
+    
 }
 
